b005276482e25f4dd735d0892096418055110e90
resulramazanli <resul.ramazanli.2008@gmail.com>
2026-03-12 17:34:27 +0400

    Update student_main.c

--- student_main.c
+++ student_main.c
@@ -6,33 +6,44 @@ int main() {
     int intersection[100];
     int count = 0;
 
-    // Read Set 1
     if (scanf("%d", &n1) != 1) return 0;
     for(i = 0; i < n1; i++) {
         scanf("%d", &set1[i]);
     }
-
-    // Read Set 2
     if (scanf("%d", &n2) != 1) return 0;
     for(i = 0; i < n2; i++) {
         scanf("%d", &set2[i]);
     }
-
-    /* YOUR CODE GOES HERE!
-         Hints:
-         1. You need to find numbers that are in BOTH set1 and set2.
-         2. BUT, only include each common number ONCE in your result.
-         3. Store the common numbers in the 'intersection' array.
-    */
-    //----------------------------------------------------------------//
-    {
-
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(set1[i] == set2[j]) {
+                int exists = 0;
+                for(k = 0; k < count; k++) {
+                    if(intersection[k] == set1[i]) {
+                        exists = 1;
+                        break;
+                    }
+                }
+                if(!exists) {
+                    intersection[count] = set1[i];
+                    count++;
+                }
+            }
+        }
     }
-    //----------------------------------------------------------------//
+    for(i = 0; i < count - 1; i++) {
+        for(j = i + 1; j < count; j++) {
+            if(intersection[i] > intersection[j]) {
+                int temp = intersection[i];
+                intersection[i] = intersection[j];
+                intersection[j] = temp;
+            }
+        }
+    }
     for(i = 0; i < count; i++) {
-        printf("%d ", intersection[i]);
+        printf("%d", intersection[i]);
+        if(i < count - 1) printf(" ");
     }
     printf("\n");
-
     return 0;
 }
